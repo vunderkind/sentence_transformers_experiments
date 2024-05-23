05f747c5ae5e1281a51a64052916a9eb35cf761f
Justin Irabor <justin.irabor@iu.org>
2024-05-23 23:33:24 +0100

    upgraded for tf

--- learning_sentence_transformers/index.py
+++ learning_sentence_transformers/index.py
@@ -1,5 +1,15 @@
 # Load model directly
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, TFAutoModel
 
-tokenizer = AutoTokenizer.from_pretrained("sentence-transformers/paraphrase-MiniLM-L6-v2")
-model = AutoModel.from_pretrained("sentence-transformers/paraphrase-MiniLM-L6-v2")
+
+MINI_LM = "sentence-transformers/paraphrase-MiniLM-L6-v2"
+
+tokenizer = AutoTokenizer.from_pretrained(MINI_LM)
+model = TFAutoModel.from_pretrained(MINI_LM)
+
+vocab_wrd2idx = tokenizer.vocab
+vocab_idx2wrd = {v: k for k, v in vocab_wrd2idx.items()}
+
+model_weights = model.get_weights()
+print(model_weights)
+# vocab_weights = model_weights[0]
